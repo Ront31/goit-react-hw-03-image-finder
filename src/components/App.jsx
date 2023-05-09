a2c96c0135abed725939af36a7a820f955060314
Ront31 <kolya.maslov.09@gmail.com>
2023-05-09 17:55:13 +0300

    little upd

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,20 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import React from 'react';
+import { Searchbar } from './Searchbar/Searchbar';
+
+export class App extends Component {
+  render() {
+    return (
+      <div
+        style={{
+          display: 'grid',
+          gridTemplateColumns: '1fr',
+          gridGap: '16px',
+          paddingBottom: '24px',
+        }}
+      >
+        <Searchbar />
+      </div>
+    );
+  }
+}
